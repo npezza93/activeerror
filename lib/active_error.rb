75b5f929ee0f798717014cb09798e472148dddc1
Nick Pezza <pezza@hey.com>
2024-01-08 21:43:34 -0500

    Require generator

--- lib/active_error.rb
+++ lib/active_error.rb
@@ -8,5 +8,7 @@ require "active_error/renderer"
 require "active_error/engine"
 require "active_error/version"
 
+require "generators/active_error/install/install_generator"
+
 module ActiveError
 end
