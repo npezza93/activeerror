6825910eee9f48d8e2edd0afbb386e4a578bb117
Nick Pezza <pezza@hey.com>
2024-01-10 11:42:58 -0500

    Fix activejob

--- lib/active_error/engine.rb
+++ lib/active_error/engine.rb
@@ -2,21 +2,23 @@
 
 module ActiveError
   class Engine < ::Rails::Engine
-    ActiveJobRequest = Struct.new(:parameters, :filtered_parameters)
+    ActiveJobRequest = Struct.new(:parameters, :filtered_parameters, :env, :url)
 
     isolate_namespace ActiveError
 
     initializer "active_error.middleware" do |app|
-      app.config.middleware.use ActiveError::Middleware
+      unless Rails.env.local?
+        app.config.middleware.use ActiveError::Middleware
 
-      ::ActiveJob::Base.class_eval do |base|
-        base.set_callback :perform, :around do |_param, block|
-          block.call
-        rescue StandardError => e
-          ActiveError::Captor.new(exception: e, request: ActiveJobRequest.new(
-            arguments, arguments
-          )).capture
-          raise e
+        ::ActiveJob::Base.class_eval do |base|
+          base.set_callback :perform, :around do |_param, block|
+            block.call
+          rescue StandardError => e
+            ActiveError::Captor.new(exception: e, request: ActiveJobRequest.new(
+              serialize, serialize
+            )).capture
+            raise e
+          end
         end
       end
     end
