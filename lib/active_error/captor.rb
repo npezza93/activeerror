7228b89e28ac53d7efea91688520ca021a182675
Nick Pezza <pezza@hey.com>
2024-01-10 10:30:14 -0500

    Allow request to be blank

--- lib/active_error/captor.rb
+++ lib/active_error/captor.rb
@@ -74,7 +74,7 @@ module ActiveError
                 slice(*ActionDispatch::Request::ENV_METHODS, "HTTP_USER_AGENT")
 
       fault.instances.create(headers:, parameters: request&.filtered_parameters,
-                             url: request.url)
+                             url: request&.url)
     end
   end
 end
