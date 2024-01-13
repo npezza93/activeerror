0a0e952e452fcf404bd7c23d06db1ab4a6a56f2d
Nick Pezza <pezza@hey.com>
2024-01-13 13:57:59 -0500

    Support unsupported serializers

--- lib/active_error/engine.rb
+++ lib/active_error/engine.rb
@@ -14,6 +14,20 @@ module ActiveError
       ActiveJob::Base.before_perform do
         Rails.error.set_context(active_error_request:
           ActiveJobRequest.new(serialize, serialize))
+      rescue ActiveJob::SerializationError
+        without_args = { 'job_class' => self.class.name,
+                         'job_id' => job_id,
+                         'provider_job_id' => provider_job_id,
+                         'queue_name' => queue_name,
+                         'priority' => priority,
+                         'executions' => executions,
+                         'exception_executions' => exception_executions,
+                         'locale' => I18n.locale.to_s,
+                         'timezone' => timezone,
+                         'enqueued_at' => Time.now.utc.iso8601(9),
+                         'scheduled_at' => scheduled_at&.utc&.iso8601(9) }
+        Rails.error.set_context(active_error_request:
+          ActiveJobRequest.new(without_args, without_args))
       end
     end
   end
