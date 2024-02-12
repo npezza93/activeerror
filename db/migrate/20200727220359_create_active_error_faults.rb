f259b7913b3260b6f51133241a02ee81c7961a8a
Nick Pezza <pezza@hey.com>
2024-02-11 21:44:50 -0500

    Fix schema

--- db/migrate/20200727220359_create_active_error_faults.rb
+++ db/migrate/20200727220359_create_active_error_faults.rb
@@ -4,14 +4,14 @@ class CreateActiveErrorFaults < ActiveRecord::Migration[7.1]
   def change # rubocop:disable Metrics/AbcSize
     create_table :active_error_faults do |t|
       t.belongs_to :cause
-      t.binary :backtrace, lmit: 512.megabytes
-      t.binary :backtrace_locations, lmit: 512.megabytes
+      t.binary :backtrace, limit: 512.megabytes
+      t.binary :backtrace_locations, limit: 512.megabytes
       t.string :klass
       t.text :message
       t.string :controller
       t.string :action
       t.integer :instances_count
-      t.text :blamed_files, lmit: 512.megabytes
+      t.text :blamed_files, limit: 512.megabytes
       t.text :options
       t.index %i(klass backtrace message)
 
