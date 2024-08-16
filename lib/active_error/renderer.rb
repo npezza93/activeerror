669cad2ca607f590ac13583abcaf2483e894900c
Nick Pezza <pezza@hey.com>
2024-08-15 21:20:13 -0400

    Cleanup

--- lib/active_error/renderer.rb
+++ lib/active_error/renderer.rb
@@ -16,27 +16,22 @@ module ActiveError
     attr_reader :exception, :instance
 
     delegate :trace_to_show, :source_to_show_id, :source_extracts,
-             :traces, to: :wrapper
+             :traces, to: :exception_wrapper
 
-    def backtrace_cleaner
-      Rails.backtrace_cleaner
-    end
-
-    def wrapper
-      @wrapper ||=
-        ActionDispatch::ExceptionWrapper.new(backtrace_cleaner, exception)
+    def exception_wrapper
+      @exception_wrapper ||=
+        ActionDispatch::ExceptionWrapper.new(Rails.backtrace_cleaner, exception)
     end
 
     def file
-      "rescues/#{wrapper.rescue_template}"
+      "rescues/#{exception_wrapper.rescue_template}"
     end
 
     def template
-      ActionDispatch::DebugView.new(
-        request: instance.request, exception_wrapper: wrapper,
-        exception: wrapper.exception, traces:,
-        show_source_idx: source_to_show_id, trace_to_show:, source_extracts:
-      )
+      ActionDispatch::DebugView.
+        new(request: instance.request, exception_wrapper:, traces:,
+            exception: exception_wrapper.exception, trace_to_show:,
+            source_extracts:, show_source_idx: source_to_show_id,)
     end
   end
 end
