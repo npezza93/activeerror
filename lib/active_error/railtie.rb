e47a6cc02e9668f8bf122e6248afd62f1f542f8e
Nick Pezza <pezza@hey.com>
2024-10-04 23:14:13 -0400

    Ignore handled

--- lib/active_error/railtie.rb
+++ lib/active_error/railtie.rb
@@ -2,8 +2,8 @@
 
 module ActiveError
   class ErrorSubscriber
-    def report(exception, context:, handled:, **_opts)
-      return if Rails.env.development? || handled ||
+    def report(exception, context:, **_opts)
+      return if Rails.env.development? ||
         ActiveError.ignored_classes.include?(exception.class.to_s)
 
       Captor.new(exception:, request: context[:active_error_request]).capture
